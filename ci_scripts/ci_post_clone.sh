ac75fdb8463f5d2180936644102f3f3f56d5ecbb
benpatterson48 <ben.patterson@underdogfantasy.com>
2025-05-08 23:06:41 -0500

    Update repo path

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,15 +1,18 @@
+
 #!/bin/sh
+
+REPO_PATH="/Volumes/workspace/repository"
 
 if [ "$CI_WORKFLOW" = "Code Coverage Check" ]
 then
   # fetch a reference to the develop branch on GitHub
   # this will allow SonarQube analysis to work
-  git -C $CI_WORKSPACE checkout -b temp
-  git -C $CI_WORKSPACE branch -d $CI_PULL_REQUEST_TARGET_BRANCH
+  git -C $REPO_PATH checkout -b temp
+  git -C $REPO_PATH branch -d $CI_PULL_REQUEST_TARGET_BRANCH
 
-  git -C $CI_WORKSPACE config remote.origin.fetch \
+  git -C $REPO_PATH config remote.origin.fetch \
     "+refs/heads/$CI_PULL_REQUEST_SOURCE_BRANCH:refs/remotes/origin/$CI_PULL_REQUEST_SOURCE_BRANCH"
-  git -C $CI_WORKSPACE config remote.origin.fetch \
+  git -C $REPO_PATH config remote.origin.fetch \
     "+refs/heads/$CI_PULL_REQUEST_TARGET_BRANCH:refs/remotes/origin/$CI_PULL_REQUEST_TARGET_BRANCH"
-  git -C $CI_WORKSPACE fetch
+  git -C $REPO_PATH fetch
 fi
